9ab78122961302a2d4a5a281a360304c010c1eed
Shwetha Nayak <64678982+shwethanayak-lang@users.noreply.github.com>
2021-03-20 12:19:56 +0530

    Create assign21.js

---
+++ assign21.js
@@ -0,0 +1,88 @@
+let chessContainer = document.getElementById("chessContainer");
+
+for (i = 1; i < 9; i++) {
+  let box1 = document.createElement("div");
+
+  if (i % 2 == 0) {
+    box1.style.backgroundColor = "black";
+  } else {
+    box1.style.backgroundColor = "white";
+  }
+  chessContainer.appendChild(box1);
+}
+for (i = 1; i < 9; i++) {
+  let box2 = document.createElement("div");
+
+  if (i % 2 == 0) {
+    box2.style.backgroundColor = "white";
+  } else {
+    box2.style.backgroundColor = "black";
+  }
+  chessContainer.appendChild(box2);
+}
+
+for (i = 1; i < 9; i++) {
+  let box3 = document.createElement("div");
+
+  if (i % 2 == 0) {
+    box3.style.backgroundColor = "black";
+  } else {
+    box3.style.backgroundColor = "white";
+  }
+  chessContainer.appendChild(box3);
+}
+
+for (i = 1; i < 9; i++) {
+  let box4 = document.createElement("div");
+
+  if (i % 2 == 0) {
+    box4.style.backgroundColor = "white";
+  } else {
+    box4.style.backgroundColor = "black";
+  }
+  chessContainer.appendChild(box4);
+}
+
+for (i = 1; i < 9; i++) {
+  let box5 = document.createElement("div");
+
+  if (i % 2 == 0) {
+    box5.style.backgroundColor = "black";
+  } else {
+    box5.style.backgroundColor = "white";
+  }
+  chessContainer.appendChild(box5);
+}
+
+for (i = 1; i < 9; i++) {
+  let box6 = document.createElement("div");
+
+  if (i % 2 == 0) {
+    box6.style.backgroundColor = "white";
+  } else {
+    box6.style.backgroundColor = "black";
+  }
+  chessContainer.appendChild(box6);
+}
+
+for (i = 1; i < 9; i++) {
+  let box7 = document.createElement("div");
+
+  if (i % 2 == 0) {
+    box7.style.backgroundColor = "black";
+  } else {
+    box7.style.backgroundColor = "white";
+  }
+  chessContainer.appendChild(box7);
+}
+
+for (i = 1; i < 9; i++) {
+  let box8 = document.createElement("div");
+
+  if (i % 2 == 0) {
+    box8.style.backgroundColor = "white";
+  } else {
+    box8.style.backgroundColor = "black";
+  }
+  chessContainer.appendChild(box8);
+}
